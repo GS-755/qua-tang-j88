6fe6bc962cf56916d299cedfe8c59b5c76ccf6f5
GS-755 <21dh114236@st.huflit.edu.vn>
2024-01-22 16:42:34 +0700

    Edit sass code to new convention

--- src/script/compile.js
+++ src/script/compile.js
@@ -1,5 +1,5 @@
 const pug = require('pug');
 const compiledFunction = pug.compileFile('index.pug');
-console.log(pug.renderFile('index.pug', {
-  name: 'qua-tang-j88'
+console.log(pug.renderFile('src/index.pug', {
+  name: 'index'
 }));
